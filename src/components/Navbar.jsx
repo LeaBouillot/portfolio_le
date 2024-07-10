8da85e1019637570702b02ea0638474b70f275a4
bestdevelope <leabouillot7@gmail.com>
2024-07-10 23:18:43 +0200

    menu burger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,9 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-white flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4 text-1">
-      <h1 className="w-full text-3xl font-bold primary-color ml-4">
-        Portfolio Léa
-      </h1>
-      <ul>
+    <div className="text-gray-500 flex justify-between items-center max-w-[1240px] mx-auto h-24 px-4 text-1">
+      <h1 className="text-3xl font-bold primary-color ml-4">PORTFOLIO LÉA</h1>
+      <ul className="hidden md:flex">
         <li className="p-2">Home</li>
         <li className="p-2">About</li>
         <li className="p-2">Skill</li>
@@ -22,7 +20,25 @@ const Navbar = () => {
       </ul>
       {/* mebu burger masqué */}
       <div onClick={handleNav} className="block md:hidden">
-        {nav ? <AiOutlineMenu size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* menu burger */}
+      <div
+        className={
+          nav
+            ? "text-gray-300 fixed h-full left-0 top-0 w-[60%] border-r-gray-900 bg-[#202121] ease-in-out duration"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="text-3xl primary-color m-4">PORTFOLIO LÉA</h1>
+        <ul className="p-8 text-2xl">
+          <li className="p-2">Home</li>
+          <li className="p-2">About</li>
+          <li className="p-2">Skill</li>
+          <li className="p-2">Projet</li>
+          <li className="p-2">Contact</li>
+        </ul>
       </div>
     </div>
   );
